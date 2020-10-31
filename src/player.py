50f3ffdd6f357d4942f58b378d81b7d1ded89672
ig <gaul@pdx.edu>
2020-10-30 19:09:05 -0700

    cleanup for merge

--- src/player.py
+++
@@ -1,7 +0,0 @@
-class Player:
-    def __init__(self, name, token):
-        self.name = name
-        self.token = token
-
-    
-
--- src/tictactoe.py
+++
@@ -1,5 +0,0 @@
-class player:
-    def __init__(self, name, board, token):
-        self.name = name
-        self.boart = board
-        self.token = token
